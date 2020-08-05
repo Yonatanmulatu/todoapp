f9127a9d3ef8d6144a318812c89f88f68c3d27d9
Yonatanmulatu <yonatan.t.mulatu@gmail.com>
2020-08-05 10:33:39 +0800

    Updates

---
+++ precache-manifest.f40a5b3f7d4e9554816182afd2cc6dbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59ca32a15ede452ff2e67aed4d0be3f",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "8858e28e6c3839d42e1b",
+    "url": "/todoapp/static/css/main.6f9335c9.chunk.css"
+  },
+  {
+    "revision": "d82b8e6f7d97f72cbf45",
+    "url": "/todoapp/static/js/2.3ea5929a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.3ea5929a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8858e28e6c3839d42e1b",
+    "url": "/todoapp/static/js/main.c20ff27d.chunk.js"
+  },
+  {
+    "revision": "9b2f915f09a204b07370",
+    "url": "/todoapp/static/js/runtime-main.d779cf41.js"
+  }
+]);
